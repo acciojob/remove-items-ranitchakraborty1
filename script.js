717fcce6fcf707249faf79e1956888576b17a8ed
ranitchakraborty1 <162864925+ranitchakraborty1@users.noreply.github.com>
2024-08-26 10:51:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let removeButton = document.getElementByID('removeButton');
+
+removeButton.addEventListener("click",myfunction);
+
+function myfunction(){
+	let colorSelect = document.getElementByID('colorSelect');
+	 const selectedColorIndex = colorSelect.selectedIndex;
+
+            if (selectedColorIndex !== -1) {
+                colorSelect.remove(selectedColorIndex);
+            }
+}
